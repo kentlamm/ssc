db8a2dff23ac8b80d9a1e311dcdd4f61930818d4
kentlamm <37251537+kentlamm@users.noreply.github.com>
2023-05-03 15:13:18 -0700

    create github pages

--- assets/witsec-mailform/mail.php
+++ assets/witsec-mailform/mail.php
@@ -33,10 +33,10 @@ $rcpVersion = "3";								// reCAPTCHA Version
 $rcpScore = "0.5";									// reCAPTCHA Score
 $rcpSecret = "";								// reCAPTCHA Secret Key
 $smtp = ("1" == "1" ? true : false);							// Use SMTP
-$smtpDebug = ("1" == "1" ? 3 : 0);						// SMTP Debug
+$smtpDebug = ("0" == "1" ? 3 : 0);						// SMTP Debug
 $smtpHost = "smtp.gmail.com";											// SMTP Host
-$smtpPort = "465";											// SMTP Port
-$smtpSecure = "ssl";										// SMTP Use SSL/TLS (empty, ssl or tls)
+$smtpPort = "587";											// SMTP Port
+$smtpSecure = "tls";										// SMTP Use SSL/TLS (empty, ssl or tls)
 $smtpUsername = "standardstoryco@gmail.com";									// SMTP Username
 $smtpPassword = "g!sh4D0w89!e";									// SMTP Password
 $attachmentsMimeTypes = explode(",", "application/gzip,application/java-archive,application/json,application/ld+json,application/msword,application/ogg,application/pdf,application/rtf,application/vnd.amazon.ebook,application/vnd.api+json,application/vnd.apple.installer+xml,application/vnd.mozilla.xul+xml,application/vnd.ms-excel,application/vnd.ms-fontobject,application/vnd.ms-powerpoint,application/vnd.oasis.opendocument.presentation,application/vnd.oasis.opendocument.spreadsheet,application/vnd.oasis.opendocument.text,application/vnd.openxmlformats-officedocument.presentationml.presentation,application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,application/vnd.openxmlformats-officedocument.wordprocessingml.document,application/vnd.rar,application/vnd.visio,application/x-7z-compressed,application/x-abiword,application/x-bzip,application/x-bzip2,application/x-freearc,application/x-httpd-php,application/x-tar,application/x-www-form-urlencoded,application/xhtml+xml,application/xml,application/zip,application/zstd,audio/*,font/*,image/*,multipart/form-data,text/plain,text/calendar,text/css,text/csv,text/rtf,video/*");	// Mime Types
