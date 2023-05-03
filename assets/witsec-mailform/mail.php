bfb82abd66cf741add4de7d4f68891560a2664df
kentlamm <37251537+kentlamm@users.noreply.github.com>
2023-05-03 14:46:12 -0700

    create github pages

--- assets/witsec-mailform/mail.php
+++ assets/witsec-mailform/mail.php
@@ -28,8 +28,8 @@ $template = "{formdata}<br><br>Date: {date}";											// Mail Template
 $autorespondSubjectPrefix = "Re:";			// Autorespond Form Subject Prefix
 $autorespondSubject = "";						// Autorespond Custom Subject
 $autorespondTemplate = "Hi {name},<br><br>Thank you for your message. I'll get back to you as soon as I can.<br>Here's the information you sent me:<br><br>{formdata}<br><br>---<br>Have a nice day.";					// Autorespond Template
-$rcp = ("1" == "1" ? true : false);						// Use reCAPTCHA
-$rcpVersion = "captcha";								// reCAPTCHA Version
+$rcp = ("0" == "1" ? true : false);						// Use reCAPTCHA
+$rcpVersion = "3";								// reCAPTCHA Version
 $rcpScore = "0.5";									// reCAPTCHA Score
 $rcpSecret = "";								// reCAPTCHA Secret Key
 $smtp = ("0" == "1" ? true : false);							// Use SMTP
